f3b49069e05a331da23d6e325ff3875daa256e83
Malte Ruff <112979235+RuffMalte@users.noreply.github.com>
2025-03-28 12:33:14 +0100

    added swiftData config

--- WorkoutMaster/WorkoutMasterApp.swift
+++ WorkoutMaster/WorkoutMasterApp.swift
@@ -6,9 +6,33 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct WorkoutMasterApp: App {
+	
+	
+	let container: ModelContainer
+	
+	init() {
+		let schema = Schema([
+			ExerciseModel.self,
+			ExerciseSetGroup.self,
+			WorkoutGroupModel.self,
+			WorkoutModel.self,
+			WorkoutSetModel.self
+		])
+		let config = ModelConfiguration("iCloud.com.MalteRuff.WorkoutMaster", schema: schema)
+		do {
+			container = try ModelContainer(for: schema, configurations: config)
+		} catch {
+			fatalError("Could not configure the container")
+		}
+		print(URL.applicationSupportDirectory.path(percentEncoded: false))
+	}
+
+	
+	
     var body: some Scene {
         WindowGroup {
             ContentView()
