69ebbeff95b6c8f49ed473f089f1128683d6ed3f
Malte Ruff <112979235+RuffMalte@users.noreply.github.com>
2025-03-28 08:55:02 +0100

    Initial Commit

---
+++ WorkoutMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkoutMaster
+//
+//  Created by Malte Ruff on 28.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutMaster/WorkoutMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutMasterApp.swift
+//  WorkoutMaster
+//
+//  Created by Malte Ruff on 28.03.25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
